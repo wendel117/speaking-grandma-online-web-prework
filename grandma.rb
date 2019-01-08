32650b7e12bd3c3a3330ee919b50d71bfb5d1fa1
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-08 17:27:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(phrase)
 if phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
 
-elsif phrase.downcase
+elsif phrase==phrase.downcase
  return "HUH?! SPEAK UP, SONNY!"
  else
  return "NO, NOT SINCE 1938!"
