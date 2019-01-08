4804c9e9800803178c124f8dabb279aef862bd34
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-08 17:33:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
 
-if phrase != phrase.uppercase
- return "HUH?! SPEAK UP, SONNY!"
-
-elsif phrase == "I LOVE YOU GRANDMA!"
+if phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
+
+elsif (phrase == phrase.downcase)
+ return "HUH?! SPEAK UP, SONNY!"
 
  else
  return "NO, NOT SINCE 1938!"
